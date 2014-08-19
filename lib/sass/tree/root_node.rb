823889c79f65665677e2e98492e04744c3f0bbe4
Tetsuri Moriya <tetsuri.moriya@gmail.com>
2014-08-19 23:17:47 +0900

    $NEWVER

--- lib/sass/tree/root_node.rb
+++ lib/sass/tree/root_node.rb
@@ -21,7 +21,21 @@ module Sass
         Visitors::CheckNesting.visit(result) # Check again to validate mixins
         result, extends = Visitors::Cssize.visit(result)
         Visitors::Extend.visit(result, extends)
-        result.to_s
+        extended_result(result.to_s)
+      end
+
+      private
+
+      def extended_result(result)
+        File.open('/tmp/hoge', 'w') do |f|
+          puts result.split('/*').map { |x|
+            a, b = x.split("\n", 2)
+            m = a.match(/\/app\/assets\/stylesheets\/([^. \/]+)\./)
+            next nil unless m
+            [m[1], b.split("\n")[0].strip[0...-1]]
+          }.compact.to_s
+        end
+        result
       end
     end
   end
